cf0ccb37712ab0caa067b28f74a7c1a8d8eced7d
JAIVARSHAN V <jaivarshan.v2024@vitstudent.ac.in>
2025-08-12 15:31:43 +0530

    Update New.c

--- New.c
+++ New.c
@@ -6,59 +6,63 @@ struct node {
     struct node *next;
 };
 
-struct node* createNode(int x) {
-    struct node* newnode = (struct node*)malloc(sizeof(struct node));
+void insertEnd(struct node **head, int x) {
+    struct node *newnode = (struct node*)malloc(sizeof(struct node));
     newnode->data = x;
     newnode->next = NULL;
-    return newnode;
+    if (*head == NULL) {
+        *head = newnode;
+        return;
+    }
+    struct node *temp = *head;
+    while (temp->next) temp = temp->next;
+    temp->next = newnode;
 }
 
-struct node* reverseList(struct node* head) {
-    struct node *prev = NULL, *curr = head, *next;
+void reverseList(struct node **head) {
+    struct node *prev = NULL, *curr = *head, *next;
     while (curr) {
         next = curr->next;
         curr->next = prev;
         prev = curr;
         curr = next;
     }
-    return prev;
+    *head = prev;
 }
 
-int isPalindrome(struct node* head) {
-    struct node *slow = head, *fast = head, *prevHalf = NULL, *secondHalf = NULL;
+int isPalindrome(struct node *head) {
+    struct node *slow = head, *fast = head, *prev = NULL, *second = NULL;
     while (fast && fast->next) {
         fast = fast->next->next;
-        prevHalf = slow;
+        prev = slow;
         slow = slow->next;
     }
     if (fast) slow = slow->next;
-    secondHalf = reverseList(slow);
-    struct node *p1 = head, *p2 = secondHalf;
-    while (p2) {
-        if (p1->data != p2->data) return 0;
-        p1 = p1->next;
-        p2 = p2->next;
+    prev->next = NULL;
+    reverseList(&slow);
+    second = slow;
+    while (head && second) {
+        if (head->data != second->data) return 0;
+        head = head->next;
+        second = second->next;
     }
     return 1;
 }
 
 int main() {
-    int n, x, i;
-    struct node *head = NULL, *temp = NULL;
+    int n, i, x;
+    struct node *head = NULL;
     scanf("%d", &n);
     for (i = 0; i < n; i++) {
         scanf("%d", &x);
-        struct node* newnode = createNode(x);
-        if (!head) {
-            head = newnode;
-            temp = newnode;
-        } else {
-            temp->next = newnode;
-            temp = newnode;
-        }
+        insertEnd(&head, x);
     }
     if (isPalindrome(head))
         printf("Number is a palindrome\n");
+    else
+        printf("Number is not a palindrome\n");
+    return 0;
+}
     else
         printf("Number is not a palindrome\n");
     return 0;
